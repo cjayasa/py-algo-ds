c7a2136aa9ba9d50cd931110a5d71fbe8810674c
Chathura Jayasanka <kalugala-muhandiramge.chathura-jayasanka.8404@student.uu.se>
2022-12-27 15:37:51 +0100

    bubble sort added

---
+++ sorting/bubble.py
@@ -0,0 +1,18 @@
+def bubble_sort(array):
+    sorted = False
+    while not sorted:
+        i = 1
+        swapped = False
+        while i < len(array):
+            if array[i-1] > array[i]:
+                array[i-1], array[i] = array[i], array[i-1]
+                swapped = True
+            i += 1
+        if not swapped:
+            break;
+    return array
+
+if __name__ == "__main__":
+    array = [4, 2, 7, 9, 1, 14, 6, 8, 2]
+    sorted_array = bubble_sort(array)
+    print(sorted_array)
